2f2c470cccecd1a763380712f7b734e77dc1adba
Amrit Utsav <lbrock513@gmail.com>
2025-11-02 16:10:32 +0530

    server-side-auth implemented

---
+++ codesight-backend/src/controllers/chat.controllers.js
@@ -0,0 +1,13 @@
+import { chatClient } from "../config/streams";
+
+export async function getStreamToken(req, res){
+    try{
+        //use clerk id for stream, not mongodb - it should match the ID in the stream dashboard.
+        const token = chatClient.createToken(req.user.clerkId);
+        res.status(200).json({token, userId: req.user.clerkId, userName:req.user.name, userImage: req.user.image});
+    }
+    catch(e){
+        console.log("Error in getStreamToken",e);
+        res.status(500).json({msg:"Internal Server Error"});
+    }
+}
