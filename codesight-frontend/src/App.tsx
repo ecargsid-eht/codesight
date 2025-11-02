d4efcb30af811212b606f34c85a5b33497bd8a5a
Amrit Utsav <lbrock513@gmail.com>
2025-11-02 18:15:44 +0530

    first commit to setup frontend tools

--- codesight-frontend/src/App.tsx
+++ codesight-frontend/src/App.tsx
@@ -1,25 +1,23 @@
 
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
+import ProblemsPage from './pages/ProblemsPage';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-
+  const {isSignedIn}=useUser();
   return (
     <>
-      <h1>Welcome to the app</h1>
-      <SignedOut>
-        <SignInButton mode='modal' >
-          <button>
-            Login
-          </button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton  />
-      </SignedIn>
-
-      <UserButton />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/problems" element={ isSignedIn ? <ProblemsPage /> : <HomePage />} />
+      </Routes>
+      <div>
+        <Toaster toastOptions={{duration:3000}}/>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- codesight-frontend/src/main.tsx
+++ codesight-frontend/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router';
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -12,8 +19,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
