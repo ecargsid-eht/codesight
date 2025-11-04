b0d10d3f5de4fb08a9e00c6a762e08453eb6cf33
Amrit Utsav <lbrock513@gmail.com>
2025-11-05 01:05:22 +0530

    redirection solved.

--- codesight-frontend/src/pages/HomePage.tsx
+++ codesight-frontend/src/pages/HomePage.tsx
@@ -36,7 +36,7 @@ function HomePage() {
           </Link>
 
           {/* AUTH BTN */}
-          <SignInButton mode="modal">
+          <SignInButton forceRedirectUrl={"/dashboard"} mode="modal">
             <button className="group px-6 py-3 bg-linear-to-r from-primary to-secondary rounded-xl text-white font-semibold text-sm shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105 flex items-center gap-2">
               <span>Get Started</span>
               <ArrowRightIcon className="size-4 group-hover:translate-x-0.5 transition-transform" />
